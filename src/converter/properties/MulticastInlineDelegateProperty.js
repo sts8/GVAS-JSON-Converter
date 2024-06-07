3263054efc9c2fb0d3b08e0f09eb587b546374a6
sts8 <104705788+sts8@users.noreply.github.com>
2024-06-07 17:47:21 +0200

    Fix MulticastInlineDelegateProperty, update lockfile

--- src/converter/properties/MulticastInlineDelegateProperty.js
+++ src/converter/properties/MulticastInlineDelegateProperty.js
@@ -1,33 +1,46 @@
 class MulticastInlineDelegateProperty {
     static padding = new Uint8Array([0x00, 0x00, 0x00, 0x00, 0x00]);
-    static unknown = new Uint8Array([0x01, 0x00, 0x00, 0x00]);
     type = "MulticastInlineDelegateProperty";
 
     constructor(name, savReader) {
         this.name = name;
         savReader.readUInt32(); // contentSize
         savReader.readBytes(5); // padding
-        savReader.readBytes(4); // unknown
-        this.object_name = savReader.readString();
-        this.function_name = savReader.readString();
+        const number_of_elements = savReader.readUInt32();
+
+        const tempMap = new Map();
+
+        for (let i = 0; i < number_of_elements; i++) {
+            const object_name = savReader.readString();
+            const function_name = savReader.readString();
+            tempMap.set(object_name, function_name);
+        }
+
+        this.elements = Array.from(tempMap.entries());
     }
 
     toBytes() {
         const {writeString, writeUint32} = require("../value-writer");
 
-        const contentSize =
-            MulticastInlineDelegateProperty.unknown.length
-            + 4 + this.object_name.length + 1
-            + 4 + this.function_name.length + 1;
+        let contentSize = 4
+        for (const [object_name, function_name] of this.elements) {
+            contentSize += 4 + object_name.length + 1;
+            contentSize += 4 + function_name.length + 1;
+        }
+
+        let byteArrayContent = new Uint8Array(0);
+
+        for (const [object_name, function_name] of this.elements) {
+            byteArrayContent = new Uint8Array([...byteArrayContent, ...writeString(object_name), ...writeString(function_name)]);
+        }
 
         return new Uint8Array([
             ...writeString(this.name),
             ...writeString(this.type),
             ...writeUint32(contentSize),
             ...MulticastInlineDelegateProperty.padding,
-            ...MulticastInlineDelegateProperty.unknown,
-            ...writeString(this.object_name),
-            ...writeString(this.function_name)
+            ...writeUint32(this.elements.length),
+            ...byteArrayContent
         ]);
     }
 }
