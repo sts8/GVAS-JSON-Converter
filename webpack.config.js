cf1f31a68f2b106895f51fe885c4885a2dd42692
sts8 <104705788+sts8@users.noreply.github.com>
2024-11-18 21:04:47 +0100

    Update dependencies; add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: {
-        index: "./src/website/main.js"
+        index: path.resolve(__dirname, 'src/website/main.js')
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/'),
+        clean: true
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist/'),
+        },
+        open: true,
+        hot: true,
+        host: 'localhost',
+        port: 8080
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/website/index.html"
+            template: path.resolve(__dirname, 'src/website/index.html')
         }),
         new CopyPlugin({
             patterns: [
-                {context: "./src/website/", from: "css/*"}
+                {
+                    context: path.resolve(__dirname, 'src/website/'),
+                    from: 'css/*',
+                    to: path.resolve(__dirname, 'dist/')
+                }
             ]
         })
-    ],
-    output: {
-        filename: "bundle.js",
-        clean: true
-    }
+    ]
 };
